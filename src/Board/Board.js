66f614a22898cfd145f1112810d9673153ed8951
Ismael Vazquez Jr <vazquezjrismael@gmail.com>
2020-05-05 16:01:09 -0700

    Building out the board functionality

--- src/Board/Board.js
+++ src/Board/Board.js
@@ -10,6 +10,55 @@ class Board extends Component {
 
     constructor(props) {
         super(props);
+
+        let board = this.generateBoard();
+
+        this.state = {
+            board: board
+        }
+
+        this.newGame = this.newGame.bind(this);
+        this.testButton = this.testButton.bind(this);
+    }
+
+    generateBoard() {
+        let board = new Array(this.props.nRows);
+        let counter = 0;
+        for (let i = 0; i < this.props.nRows; i++) {
+            board[i] = new Array(this.props.nCols);
+            for (let j = 0; j < this.props.nCols; j++) {
+                counter++;
+                if (Math.floor(Math.random() * 4))
+                    board[i][j] = <Cell key={counter} id={counter} isLit={false} test={this.testButton}/>;
+                else 
+                    board[i][j] = <Cell key={counter} id={counter} isLit={true} test={this.testButton}/>;
+            }
+        }
+        return board;
+    }
+
+    newGame() {
+        let board = this.generateBoard();
+        this.setState ({
+            board:board
+        });
+    }
+
+    testButton(cell, isLit) {
+        let board = this.state.board.map(row => row.slice());
+        let counter = 0;
+        for (let i = 0; i < board.length; i++) {
+            for (let j = 0; j < board[i].length; j++) {
+                counter++;
+                if (cell === counter) {
+                    board[i][j] = <Cell key={counter} id={counter} isLit={!isLit} test={this.testButton}/>;
+                }
+            }
+        }
+        
+        this.setState({
+            board: board
+        })
     }
 
     render() {
@@ -20,44 +69,10 @@ class Board extends Component {
                 </div>
                 <table>
                     <tbody>
-                        <tr>
-                            <td><Cell status="on"/></td>
-                            <td><Cell status="on"/></td>
-                            <td><Cell status="off"/></td>
-                            <td><Cell status="on"/></td>
-                            <td><Cell status="off"/></td>
-                        </tr>
-                        <tr>
-                            <td><Cell status="off"/></td>
-                            <td><Cell status="off"/></td>
-                            <td><Cell status="off"/></td>
-                            <td><Cell status="on"/></td>
-                            <td><Cell status="off"/></td>
-                        </tr>
-                        <tr>
-                            <td><Cell status="off"/></td>
-                            <td><Cell status="on"/></td>
-                            <td><Cell status="on"/></td>
-                            <td><Cell status="off"/></td>
-                            <td><Cell status="off"/></td>
-                        </tr>
-                        <tr>
-                            <td><Cell status="off"/></td>
-                            <td><Cell status="off"/></td>
-                            <td><Cell status="off"/></td>
-                            <td><Cell status="on"/></td>
-                            <td><Cell status="off"/></td>
-                        </tr>
-                        <tr>
-                            <td><Cell status="off"/></td>
-                            <td><Cell status="on"/></td>
-                            <td><Cell status="off"/></td>
-                            <td><Cell status="on"/></td>
-                            <td><Cell status="off"/></td>
-                        </tr>
+                        <tr>{this.state.board.map((row, i) => <td key={i}>{row}</td>)}</tr>
                     </tbody>
                 </table>
-                <button className="new-game-button">New Game</button>
+                <button className="new-game-button" onClick={this.newGame}>New Game</button>
             </div>
         );
     }
--- src/Cell/Cell.js
+++ src/Cell/Cell.js
@@ -2,9 +2,19 @@ import React, { Component } from 'react';
 import './Cell.css';
 
 class Cell extends Component {
+    constructor(props) {
+        super(props);
+        this.testHandler = this.testHandler.bind(this);
+    }
+
+    testHandler() {
+        this.props.test(this.props.id, this.props.isLit);
+    }
+
     render() {
+        let isLit = this.props.isLit ? "on" : "off";
         return (
-            <div className={`cell ${this.props.status}`}>
+            <div className={`cell ${isLit}`} onClick={this.testHandler}>
 
             </div>
         );
